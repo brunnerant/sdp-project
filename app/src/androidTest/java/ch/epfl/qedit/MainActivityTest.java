1bfae334a374859fce3c80b74022a069abe90507
Antoine Brunner <antoine.brunner@epfl.ch>
2020-02-27 21:24:49 +0100

    Add support for formatting through spotless

--- app/src/androidTest/java/ch/epfl/qedit/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/qedit/MainActivityTest.java
@@ -1,16 +1,15 @@
 package ch.epfl.qedit;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
--- app/src/main/java/ch/epfl/qedit/MainActivity.java
+++ app/src/main/java/ch/epfl/qedit/MainActivity.java
@@ -1,8 +1,7 @@
 package ch.epfl.qedit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
