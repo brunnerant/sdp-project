f4b59b3038ecc70dfe1d354b2121890e972ef38b
Nicolas Bähler <nicolas.bahler@epfl.ch>
2020-03-01 00:23:21 +0100

    First version of an interface between frontend and backend. Additionally introduces User, Role and Status

---
+++ app/src/main/java/ch/epfl/qedit/backend/Firebase.java
@@ -0,0 +1,15 @@
+package ch.epfl.qedit.backend;
+
+import ch.epfl.qedit.Request;
+import ch.epfl.qedit.Response;
+
+public class Firebase extends Backend {
+    public Firebase() {
+        super();
+    }
+
+    @Override
+    public Response sendRequest(Request request) {
+        return null;
+    }
+}
--- app/src/main/java/ch/epfl/qedit/frontend/MainActivity.java
+++ app/src/main/java/ch/epfl/qedit/frontend/MainActivity.java
@@ -1,7 +1,8 @@
-package ch.epfl.qedit;
+package ch.epfl.qedit.frontend;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import ch.epfl.qedit.R;
 
 public class MainActivity extends AppCompatActivity {
 
