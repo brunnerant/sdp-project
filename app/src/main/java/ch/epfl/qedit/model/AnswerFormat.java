a353642c5329947a98345c6f580d552d55963f77
Antoine Brunner <antoine.brunner@epfl.ch>
2020-03-01 17:55:03 +0100

    Factorize integer and float fields into one class

--- app/src/main/java/ch/epfl/qedit/model/AnswerFormat.java
+++ app/src/main/java/ch/epfl/qedit/model/AnswerFormat.java
@@ -6,55 +6,30 @@ public abstract class AnswerFormat {
     /** This method is used to implement the visitor pattern */
     public abstract void accept(Visitor visitor);
 
-    public static class IntegerField extends AnswerFormat {
-        private final int min;
-        private final int max;
+    public static class NumberField<T extends Number> extends AnswerFormat {
+        private final T min;
+        private final T max;
 
-        public IntegerField(int min, int max) {
+        public NumberField(T min, T max) {
             this.min = min;
             this.max = max;
         }
 
-        public int getMin() {
+        public T getMin() {
             return min;
         }
 
-        public int getMax() {
+        public T getMax() {
             return max;
         }
 
         @Override
         public void accept(Visitor visitor) {
-            visitor.visitIntegerField(this);
-        }
-    }
-
-    public static class FloatField extends AnswerFormat {
-        private final float min;
-        private final float max;
-
-        public FloatField(float min, float max) {
-            this.min = min;
-            this.max = max;
-        }
-
-        public float getMin() {
-            return min;
-        }
-
-        public float getMax() {
-            return max;
-        }
-
-        @Override
-        public void accept(Visitor visitor) {
-            visitor.visitFloatField(this);
+            visitor.visitNumberField(this);
         }
     }
 
     public interface Visitor {
-        public void visitIntegerField(IntegerField field);
-
-        public void visitFloatField(FloatField field);
+        public <T extends Number> void visitNumberField(NumberField<T> field);
     }
 }
