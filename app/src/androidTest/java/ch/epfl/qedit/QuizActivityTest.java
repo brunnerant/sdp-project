2a644dc208553941ea3ed715542931b4033f6c19
nathan96g <nathangreslin@gmail.com>
2020-03-04 19:50:34 +0100

    Test for the Quiz activity

---
+++ app/src/androidTest/java/ch/epfl/qedit/QuizActivityTest.java
@@ -0,0 +1,31 @@
+package ch.epfl.qedit;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import ch.epfl.qedit.view.QuizActivity;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class QuizActivityTest {
+
+    @Rule
+    public final ActivityTestRule<QuizActivity> mActivityRule =
+            new ActivityTestRule<>(QuizActivity.class);
+
+    @Test
+    public void testDisplayTheRightTitle() {
+        onView(withId(R.id.question_title)).check(matches(withText("1) Question test")));
+    }
+
+    @Test
+    public void testDisplayTheRightText() {
+        onView(withId(R.id.question_display)).check(matches(withText("Is this question working?")));
+    }
+}
