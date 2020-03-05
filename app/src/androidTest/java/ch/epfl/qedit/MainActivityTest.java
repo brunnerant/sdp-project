3694a2668d7ccfd3935b8e14806b8f17ef37f687
nathan96g <nathangreslin@gmail.com>
2020-03-05 20:58:54 +0100

    import added in MainActivityTest

--- app/src/androidTest/java/ch/epfl/qedit/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/qedit/MainActivityTest.java
@@ -12,6 +12,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import ch.epfl.qedit.view.MainActivity;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
