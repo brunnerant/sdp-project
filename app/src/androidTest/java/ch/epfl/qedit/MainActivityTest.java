245cfc5d53f5a177c1c4bc4b329c95c390436105
nathan96g <nathangreslin@gmail.com>
2020-03-05 20:36:32 +0100

    Spotless Apply run

--- app/src/androidTest/java/ch/epfl/qedit/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/qedit/MainActivityTest.java
@@ -1,15 +1,9 @@
 package ch.epfl.qedit;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import ch.epfl.qedit.view.MainActivity;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
@@ -17,5 +11,4 @@ public class MainActivityTest {
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
-
 }
