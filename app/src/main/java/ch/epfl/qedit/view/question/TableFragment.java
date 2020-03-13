cd8fdf515e06aeca602c61d2250e89438e4fe2d3
Alexis <alexis.p.cohen@gmail.com>
2020-03-13 01:24:24 +0100

    changed requested changes

--- app/src/main/java/ch/epfl/qedit/view/question/TableFragment.java
+++ app/src/main/java/ch/epfl/qedit/view/question/TableFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import ch.epfl.qedit.R;
 import java.util.ArrayList;
@@ -18,8 +17,6 @@ public class TableFragment extends Fragment {
     private int sizeX;
     private int sizeY;
     private List<View> answers;
-
-    private Button buttonSubmit;
 
     public TableFragment() {
         // Required empty public constructor
@@ -44,10 +41,6 @@ public class TableFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_table, container, false);
-
-        // fragment numbers are not in order as they were put in the layout in a different order
-        // still, 1st element of array is 1st element of matrix and last element of array is last
-        // element of matrix
 
         return view;
     }
