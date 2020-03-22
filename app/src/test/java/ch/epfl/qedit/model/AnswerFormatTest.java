e27b76380fcefc27f22a8a8dc5a14920b1b7b8b0
nathan96g <nathangreslin@gmail.com>
2020-03-23 00:28:29 +0100

    Test added for parsing answer_format string into AnswerFormat

--- app/src/test/java/ch/epfl/qedit/model/AnswerFormatTest.java
+++ app/src/test/java/ch/epfl/qedit/model/AnswerFormatTest.java
@@ -1,3 +1,22 @@
 package ch.epfl.qedit.model;
 
-public class AnswerFormatTest {}
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class AnswerFormatTest {
+
+    @Test
+    public void parseWrong() {
+        assertNull(AnswerFormat.parse("iufbziub"));
+        assertNull(AnswerFormat.parse("matrix45x8x6"));
+        assertNull(AnswerFormat.parse(""));
+        assertNull(AnswerFormat.parse(null));
+    }
+
+    @Test
+    public void parseMatrix() {
+        assertEquals(new MatrixFormat(1, 1), AnswerFormat.parse("matrix1x1"));
+    }
+}
