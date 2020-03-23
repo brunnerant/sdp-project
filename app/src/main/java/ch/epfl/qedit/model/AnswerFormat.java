d4606c1ac02a538bbd61a921fa7d59936050d5ce
nathan96g <nathangreslin@gmail.com>
2020-03-23 10:41:51 +0100

    spotlassApply

--- app/src/main/java/ch/epfl/qedit/model/AnswerFormat.java
+++ app/src/main/java/ch/epfl/qedit/model/AnswerFormat.java
@@ -13,14 +13,13 @@ public abstract class AnswerFormat implements Serializable {
         void visitMatrixAnswerFormat(MatrixFormat matrixFormat);
     }
 
-
     /**
      * Parse AnswerFormat from a string answer_format, call the override parse method in child
      * classes
      *
      * @param format string to parse into a AnswerFormat
-     * @return AnswerFormat corresponding to the parsed format string, or null if the string is
-     * not a correct format
+     * @return AnswerFormat corresponding to the parsed format string, or null if the string is not
+     *     a correct format
      */
     public static AnswerFormat parse(String format) {
         if (format == null) return null;
