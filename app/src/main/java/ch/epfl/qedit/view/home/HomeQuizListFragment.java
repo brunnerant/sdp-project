3d449ab03f14fc3bc7fce0e424f8bf99390e80e1
cjordan7 <cosmehbvjordan@gmail.com>
2020-03-25 19:06:55 +0100

    Refactorization for code climate

--- app/src/main/java/ch/epfl/qedit/view/home/HomeQuizListFragment.java
+++ app/src/main/java/ch/epfl/qedit/view/home/HomeQuizListFragment.java
@@ -65,7 +65,10 @@ public class HomeQuizListFragment extends Fragment {
                 new ItemTouchHelper.SimpleCallback(
                         0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
                     @Override
-                    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                    public boolean onMove(
+                            @NonNull RecyclerView recyclerView,
+                            @NonNull RecyclerView.ViewHolder viewHolder,
+                            @NonNull RecyclerView.ViewHolder target) {
                         return true;
                     }
 
@@ -75,17 +78,30 @@ public class HomeQuizListFragment extends Fragment {
                     }
 
                     @Override
-                    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                    public void onChildDrawOver(
+                            Canvas c,
+                            RecyclerView recyclerView,
+                            RecyclerView.ViewHolder viewHolder,
+                            float dX,
+                            float dY,
                             int actionState,
                             boolean isCurrentlyActive) {
                         CustomAdapter.CustomViewHolder customViewHolder =
                                 (CustomAdapter.CustomViewHolder) viewHolder;
                         getDefaultUIUtil()
-                                .onDrawOver(c, recyclerView, customViewHolder.name, dX, dY, actionState, isCurrentlyActive);
+                                .onDrawOver(
+                                        c,
+                                        recyclerView,
+                                        customViewHolder.name,
+                                        dX,
+                                        dY,
+                                        actionState,
+                                        isCurrentlyActive);
                     }
 
                     @Override
-                    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+                    public void onSelectedChanged(
+                            RecyclerView.ViewHolder viewHolder, int actionState) {
                         if (viewHolder != null) {
                             CustomAdapter.CustomViewHolder customViewHolder =
                                     (CustomAdapter.CustomViewHolder) viewHolder;
@@ -94,11 +110,26 @@ public class HomeQuizListFragment extends Fragment {
                     }
 
                     @Override
-                    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                    public void onChildDraw(
+                            Canvas c,
+                            RecyclerView recyclerView,
+                            RecyclerView.ViewHolder viewHolder,
+                            float dX,
+                            float dY,
+                            int actionState,
+                            boolean isCurrentlyActive) {
                         CustomAdapter.CustomViewHolder customViewHolder =
                                 (CustomAdapter.CustomViewHolder) viewHolder;
                         setVisibilityOnChildDraw(dX, customViewHolder);
-                        getDefaultUIUtil().onDraw(c, recyclerView, customViewHolder.name, dX, dY, actionState, isCurrentlyActive);
+                        getDefaultUIUtil()
+                                .onDraw(
+                                        c,
+                                        recyclerView,
+                                        customViewHolder.name,
+                                        dX,
+                                        dY,
+                                        actionState,
+                                        isCurrentlyActive);
                     }
                 };
 
