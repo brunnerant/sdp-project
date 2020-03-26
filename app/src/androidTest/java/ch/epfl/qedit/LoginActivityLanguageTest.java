a27d27fb588145c2a5db6b9ce18c9ef86510fb27
Anthony Iozzia <anthony.iozzia@epfl.ch>
2020-03-26 03:40:17 +0100

    tests done

---
+++ app/src/androidTest/java/ch/epfl/qedit/LoginActivityLanguageTest.java
@@ -0,0 +1,77 @@
+package ch.epfl.qedit;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.Is.is;
+
+import android.content.Intent;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import ch.epfl.qedit.view.LoginActivity;
+import java.util.Locale;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class LoginActivityLanguageTest {
+    @Rule
+    public final IntentsTestRule<LoginActivity> testRule =
+            new IntentsTestRule<>(LoginActivity.class, false, false);
+
+    @Before
+    public void launchActivity() {
+        Intent intent = new Intent();
+        testRule.launchActivity(intent);
+    }
+
+    @After
+    public void finishActivity() {
+        testRule.finishActivity();
+    }
+
+    private void testChangeLanguage(
+            String language,
+            String languageCode,
+            int pos,
+            String loginString,
+            String languageChangedString) {
+        String startupLanguage = Locale.getDefault().getLanguage();
+        onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
+        onView(withId(R.id.language_selection)).perform(click());
+        onData(anything()).atPosition(pos).perform(click());
+        onView(withId(R.id.language_selection)).check(matches(withSpinnerText(language)));
+        onView(withId(R.id.login_button)).check(matches(withText(loginString)));
+
+        // Test toast
+        if (startupLanguage != languageCode) {
+            onView(withText(languageChangedString))
+                    .inRoot(
+                            withDecorView(
+                                    not(is(testRule.getActivity().getWindow().getDecorView()))))
+                    .check(matches(isDisplayed()));
+        }
+    }
+
+    @Test
+    public void testChangeLanguageEnglish() {
+        testChangeLanguage("English", "en", 0, "Login", "Language changed to English");
+    }
+
+    @Test
+    public void testChangeLanguageFrench() {
+        testChangeLanguage("Français", "fr", 1, "Connexion", "Langue changée en Français");
+    }
+}
