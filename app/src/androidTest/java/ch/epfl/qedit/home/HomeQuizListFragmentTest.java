b4fc46250f1c985ae50f2c7cd7bc5a37d2022571
cjordan7 <cosmehbvjordan@gmail.com>
2020-03-26 19:21:40 +0100

    Spotless apply

--- app/src/androidTest/java/ch/epfl/qedit/home/HomeQuizListFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/qedit/home/HomeQuizListFragmentTest.java
@@ -1,50 +1,13 @@
 package ch.epfl.qedit.home;
 
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import androidx.test.espresso.ViewInteraction;
-
 import androidx.test.espresso.intent.Intents;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import ch.epfl.qedit.R;
 import ch.epfl.qedit.view.home.HomeQuizListFragment;
-import ch.epfl.qedit.view.quiz.QuizActivity;
 import com.android21buttons.fragmenttestrule.FragmentTestRule;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static ch.epfl.qedit.util.DragAndDropAction.dragAndDrop;
-import static ch.epfl.qedit.util.RecyclerViewMatcher.withRecyclerView;
-import static org.hamcrest.Matchers.not;
-
-import androidx.test.espresso.ViewInteraction;
-import ch.epfl.qedit.R;
-import ch.epfl.qedit.view.edit.EditOverviewFragment;
-import com.android21buttons.fragmenttestrule.FragmentTestRule;
-import org.junit.Rule;
-import org.junit.Test;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class HomeQuizListFragmentTest extends HomeFragmentsTestUsingDB {
