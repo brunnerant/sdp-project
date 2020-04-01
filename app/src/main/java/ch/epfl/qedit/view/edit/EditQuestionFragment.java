a2de9da4091a641d4256b4f73b5ae0d02c95e0ed
cjordan7 <cosmehbvjordan@gmail.com>
2020-04-01 18:34:46 +0200

    Changes

--- app/src/main/java/ch/epfl/qedit/view/edit/EditQuestionFragment.java
+++ app/src/main/java/ch/epfl/qedit/view/edit/EditQuestionFragment.java
@@ -12,16 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import ch.epfl.qedit.R;
 import ch.epfl.qedit.model.MatrixFormat;
 import ch.epfl.qedit.model.Question;
 import ch.epfl.qedit.model.Quiz;
+import ch.epfl.qedit.R;
 import ch.epfl.qedit.view.answer.MatrixFragment;
 import ch.epfl.qedit.viewmodel.QuizViewModel;
 
 public class EditQuestionFragment extends Fragment {
-    private EditText editQuestionTitle;
     private EditText editQuestionDisplay;
+    private EditText editQuestionTitle;
 
     @Nullable
     @Override
@@ -31,8 +31,8 @@ public class EditQuestionFragment extends Fragment {
             @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.edit_fragment_quiz_question, container, false);
-        editQuestionTitle = view.findViewById(R.id.edit_question_title);
         editQuestionDisplay = view.findViewById(R.id.edit_question_display);
+        editQuestionTitle = view.findViewById(R.id.edit_question_title);
 
         final QuizViewModel model =
                 new ViewModelProvider(requireActivity()).get(QuizViewModel.class);
@@ -52,27 +52,23 @@ public class EditQuestionFragment extends Fragment {
 
     /** Handles the transition from one question to another */
     private void editOnQuestionChanged(Quiz quiz, Integer index) {
-        if (index == null || quiz == null || index < 0 || index >= quiz.getQuestions().size())
-            return;
-
         Question question = quiz.getQuestions().get(index);
 
         // We have to change the question title and text
         String questionTitleStr = "Question " + (index + 1) + " - " + question.getTitle();
-        editQuestionTitle.setText(questionTitleStr);
         editQuestionDisplay.setText(question.getText());
+        editQuestionTitle.setText(questionTitleStr);
 
         MatrixFragment matrixFragment = new MatrixFragment();
-        MatrixFormat matrixFormat = (MatrixFormat) question.getFormat();
         Bundle newB = new Bundle();
-        newB.putSerializable(MatrixFragment.MATRIXID, matrixFormat);
+        newB.putSerializable(MatrixFragment.MATRIXID, question.getFormat());
         matrixFragment.setArguments(newB);
 
         // And dynamically instantiate the answer form
         requireActivity()
                 .getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.answer_fragment_container, matrixFragment)
+                .replace(R.id.edit_answer_fragment_container, matrixFragment)
                 .commit();
     }
 }
