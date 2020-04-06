4e9d6b7c7edd8c9bbaad834e547498e588eea29f
Antoine Brunner <antoine.brunner@epfl.ch>
2020-04-06 10:04:33 +0200

    Run spotless, in hope that the Travis build reports its status now

--- app/src/main/java/ch/epfl/qedit/model/answer/AnswerModel.java
+++ app/src/main/java/ch/epfl/qedit/model/answer/AnswerModel.java
@@ -3,8 +3,8 @@ package ch.epfl.qedit.model.answer;
 import java.io.Serializable;
 
 /**
- * This class represents the answer that a user entered into a question. Its subclasses
- * are in a one-to-one correspondence with the types of answers represented by the
- * AnswerFormat class hierarchy.
+ * This class represents the answer that a user entered into a question. Its subclasses are in a
+ * one-to-one correspondence with the types of answers represented by the AnswerFormat class
+ * hierarchy.
  */
 public abstract class AnswerModel implements Serializable {}
