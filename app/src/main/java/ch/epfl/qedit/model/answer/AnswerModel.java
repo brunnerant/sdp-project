6f94088e519f5f5df2fad370cb30aa8613178976
brunnerant <33020895+brunnerant@users.noreply.github.com>
2020-04-06 09:20:54 +0200

    Made another dummy commit to refresh codeclimate ;(

--- app/src/main/java/ch/epfl/qedit/model/answer/AnswerModel.java
+++ app/src/main/java/ch/epfl/qedit/model/answer/AnswerModel.java
@@ -3,8 +3,8 @@ package ch.epfl.qedit.model.answer;
 import java.io.Serializable;
 
 /**
- * This class represents the answer that a user entered into a question. The subclasses of this
- * class are in a one-to-one correspondence with the types of answers represented by the
+ * This class represents the answer that a user entered into a question. Its subclasses
+ * are in a one-to-one correspondence with the types of answers represented by the
  * AnswerFormat class hierarchy.
  */
 public abstract class AnswerModel implements Serializable {}
