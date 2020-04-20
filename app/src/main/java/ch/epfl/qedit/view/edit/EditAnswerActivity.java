e90564d511e46d770e26c54a17e90f0e4e1e6821
nathan96g <nathangreslin@gmail.com>
2020-04-20 13:17:05 +0200

    Image for the EditAnswerActivity added

---
+++ app/src/main/java/ch/epfl/qedit/view/edit/EditAnswerActivity.java
@@ -0,0 +1,16 @@
+package ch.epfl.qedit.view.edit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.epfl.qedit.R;
+
+public class EditAnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_answer);
+    }
+}
