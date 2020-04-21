e36d6a7ba93de1c46d8407aee3ecc455b168fe65
Nicolas Bähler <nicolas.bahler@epfl.ch>
2020-04-21 11:04:04 +0200

    Spotless apply, simplify EditQuizActivity, add the new quiz Settings Activities first version, start modification of EditOverview

--- app/src/main/java/ch/epfl/qedit/model/StringPool.java
+++ app/src/main/java/ch/epfl/qedit/model/StringPool.java
@@ -1,47 +1,46 @@
 package ch.epfl.qedit.model;
 
 import com.google.common.collect.ImmutableMap;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class StringPool {
 
-    public final static  String TITLE_ID = "title";
+    public static final String TITLE_ID = "title";
     private Map<String, String> stringPool;
 
-    public StringPool(){
+    public StringPool() {
         stringPool = new HashMap<>();
     }
 
-    public StringPool(Map<String, String> stringPool){
+    public StringPool(Map<String, String> stringPool) {
         this.stringPool = new HashMap<>(stringPool);
     }
 
-    private String newUID(){
+    private String newUID() {
         String id = UUID.randomUUID().toString();
-        while(stringPool.containsKey(id)){
+        while (stringPool.containsKey(id)) {
             id = UUID.randomUUID().toString();
         }
         return id;
     }
 
-    public String put(String text){
+    public String put(String text) {
         String id = newUID();
         stringPool.put(id, text);
         return id;
     }
 
-    public String put(String key, String text){
+    public String put(String key, String text) {
         return stringPool.put(key, text);
     }
 
-    public String get(String key){
+    public String get(String key) {
         return stringPool.get(key);
     }
 
-    public ImmutableMap<String, String> get(){
+    public ImmutableMap<String, String> get() {
         return ImmutableMap.copyOf(stringPool);
     }
 }
