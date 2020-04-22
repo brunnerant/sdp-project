c6a9e484c37cc100dce64e35ca3d95acb083ada5
nathan96g <nathangreslin@gmail.com>
2020-04-22 11:29:59 +0200

    layouts updated

--- app/src/main/java/ch/epfl/qedit/view/edit/EditQuizActivity.java
+++ app/src/main/java/ch/epfl/qedit/view/edit/EditQuizActivity.java
@@ -3,9 +3,16 @@ package ch.epfl.qedit.view.edit;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import ch.epfl.qedit.R;
+import ch.epfl.qedit.model.Quiz;
 import ch.epfl.qedit.view.quiz.QuestionFragment;
+import ch.epfl.qedit.viewmodel.QuizViewModel;
 
 public class EditQuizActivity extends AppCompatActivity {
+
+    public final static String QUESTION_BUILDER= "ch.epfl.qedit.view.edit.QUESTION_BUILDER";
+
+    private QuizViewModel model;
+    private Quiz quiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
