18463f2a56b7422edb626bb3edcb20557be14e9c
nathan96g <nathangreslin@gmail.com>
2020-05-05 18:25:59 +0200

    give little feedback when a good solution is specified

--- app/src/main/java/ch/epfl/qedit/view/edit/EditQuestionActivity.java
+++ app/src/main/java/ch/epfl/qedit/view/edit/EditQuestionActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -26,10 +25,13 @@ public class EditQuestionActivity extends AppCompatActivity {
 
     public static final String SOL_DIALOG_TAG = "ch.epfl.qedit.view.edit.EDIT_SOL_DIALOG_TAG";
 
-    private String title;
-    private String text;
+    private String titleId;
+    private String textId;
     private StringPool stringPool;
     private AnswerFormat answerFormat;
+
+    private EditText titleView;
+    private EditText textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +39,10 @@ public class EditQuestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_question);
 
         // Initialize the EditText fields
-        setEditTextListener(true);
-        setEditTextListener(false);
+        titleView = findViewById(R.id.edit_question_title);
+        textView = findViewById(R.id.edit_question_text);
+        titleView.addTextChangedListener(editTextListener(true));
+        textView.addTextChangedListener(editTextListener(false));
 
         // Initialize buttons
         ImageButton numButton = findViewById(R.id.number_button);
@@ -49,37 +53,51 @@ public class EditQuestionActivity extends AppCompatActivity {
         setDoneButtonListener();
         setCancelButtonListener();
 
-        // Get the StringPool from the Intent
-        Intent intent = getIntent();
-        stringPool =
-                (StringPool)
-                        Objects.requireNonNull(intent.getExtras()).getSerializable(STRING_POOL);
+        // Get the StringPool, the title and the text from the Intent
+        extractFromIntent();
+    }
+
+    /** Extract string pool and question attributes to modify */
+    private void extractFromIntent() {
+        Bundle bundle = Objects.requireNonNull(getIntent().getExtras());
+        stringPool = (StringPool) bundle.getSerializable(STRING_POOL);
+        Question question = (Question) bundle.getSerializable(QUESTION);
+
+        // If we are modifying a question, pre-set title, text and answerFormat
+        if (question != null) {
+            titleId = question.getTitle();
+            textId = question.getText();
+            titleView.setText(stringPool.get(titleId));
+            textView.setText(stringPool.get(textId));
+            setAnswerFormat(question.getFormat());
+        }
     }
 
     /**
-     * Create a listener for either the title view or the text view
+     * Create a listener for either the title view or the text view (factorise)
      *
-     * @param setTitle select if we set listener for title or text
+     * @param title select if we set listener for title or text
      */
-    private void setEditTextListener(final boolean setTitle) {
-        int id = setTitle ? R.id.edit_question_title : R.id.edit_question_text;
-        EditText editTitle = findViewById(id);
-        editTitle.addTextChangedListener(
-                new TextWatcher() {
+    private TextWatcher editTextListener(final boolean title) {
+        return new TextWatcher() {
 
-                    @Override
-                    public void beforeTextChanged(
-                            CharSequence s, int start, int count, int after) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        if (setTitle) title = s.toString();
-                        else text = s.toString();
-                    }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (title) {
+                    if (titleId == null) titleId = stringPool.add(s.toString());
+                    else stringPool.update(titleId, s.toString());
+                } else {
+                    if (textId == null) textId = stringPool.add(s.toString());
+                    else stringPool.update(textId, s.toString());
+                }
+            }
 
-                    @Override
-                    public void afterTextChanged(Editable s) {}
-                });
+            @Override
+            public void afterTextChanged(Editable s) {}
+        };
     }
 
     /** Handles the setup of the two buttons of this activity */
@@ -112,7 +130,8 @@ public class EditQuestionActivity extends AppCompatActivity {
     public void setAnswerFormat(AnswerFormat answerFormat) {
         if (answerFormat != null) {
             TextView helper = findViewById(R.id.choose_answer_text);
-            helper.setVisibility(View.GONE);
+            helper.setText(R.string.correct_answer_specified);
+            helper.setError(null);
             this.answerFormat = answerFormat;
         }
     }
@@ -124,8 +143,8 @@ public class EditQuestionActivity extends AppCompatActivity {
     private void returnResult() {
         // test if title, text and answerFormat are non-empty
         // use & operator because we want to evaluate both side
-        boolean noError = setErrorIfEmpty(title, R.id.edit_question_title);
-        noError &= setErrorIfEmpty(text, R.id.edit_question_text);
+        boolean noError = setErrorIfEmpty(titleId, R.id.edit_question_title);
+        noError &= setErrorIfEmpty(textId, R.id.edit_question_text);
         if (answerFormat == null) {
             noError = false;
             TextView answerView = findViewById(R.id.choose_answer_text);
@@ -133,8 +152,7 @@ public class EditQuestionActivity extends AppCompatActivity {
         }
         if (noError) {
             // return the Question created by this activity to the callee activity
-            Question question =
-                    new Question(stringPool.add(title), stringPool.add(text), answerFormat);
+            Question question = new Question(titleId, textId, answerFormat);
             Intent intent = new Intent();
             intent.putExtra(QUESTION, question);
             intent.putExtra(STRING_POOL, stringPool);
@@ -152,7 +170,7 @@ public class EditQuestionActivity extends AppCompatActivity {
      * @return true if there is no error set, false otherwise
      */
     private boolean setErrorIfEmpty(String str, int strViewId) {
-        if (str == null || str.isEmpty()) {
+        if (str == null || stringPool.get(str) == null || stringPool.get(str).isEmpty()) {
             EditText strView = findViewById(strViewId);
             strView.setError(getString(R.string.cannot_be_empty));
             return false;
