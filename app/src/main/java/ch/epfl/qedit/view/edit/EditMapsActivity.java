77d043c9c2923ec407c31a51cf651c25efd975c6
cjordan7 <cosmehbvjordan@gmail.com>
2020-05-13 22:19:42 +0200

    Improve Map

--- app/src/main/java/ch/epfl/qedit/view/edit/EditMapsActivity.java
+++ app/src/main/java/ch/epfl/qedit/view/edit/EditMapsActivity.java
@@ -2,8 +2,11 @@ package ch.epfl.qedit.view.edit;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,22 +14,36 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Objects;
+
 import ch.epfl.qedit.R;
+import ch.epfl.qedit.model.User;
+
+import static ch.epfl.qedit.view.LoginActivity.USER;
 
 public class EditMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    public static final String LATITUDE = "ch.epfl.qedit.view.edit.LATITUDE";
+    public static final String LONGITUDE = "ch.epfl.qedit.view.edit.LONGITUDE";
 
     private GoogleMap mMap;
+    private LatLng latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        Intent intent = getIntent();
+        double latitude = Objects.requireNonNull(intent.getExtras()).getDouble(LATITUDE);
+        double longitude = Objects.requireNonNull(intent.getExtras()).getDouble(LONGITUDE);
+        latLng = new LatLng(latitude, longitude);
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -40,10 +57,24 @@ public class EditMapsActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng location = new LatLng(46.518819, 6.561919);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(location).title("Marker in EPFL"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(location, 12);
+        mMap.animateCamera(yourLocation);
+
+        mMap.setOnMapClickListener(point -> {
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(point));
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.map_menu, menu);
+        return true;
+    }
+
 }
