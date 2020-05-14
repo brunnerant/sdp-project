a70d900af6850c770efc89acd2731effed4e2e14
cjordan7 <cosmehbvjordan@gmail.com>
2020-05-14 23:08:50 +0200

    Some tests

--- app/src/androidTest/java/ch/epfl/qedit/edit/EditMapsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/qedit/edit/EditMapsActivityTest.java
@@ -1,28 +1,16 @@
 package ch.epfl.qedit.edit;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-
+import ch.epfl.qedit.view.edit.EditMapsActivity;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import ch.epfl.qedit.R;
-import ch.epfl.qedit.model.StringPool;
-import ch.epfl.qedit.view.edit.EditMapsActivity;
-import ch.epfl.qedit.view.edit.EditSettingsActivity;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static ch.epfl.qedit.model.StringPool.TITLE_ID;
-import static ch.epfl.qedit.view.home.HomeQuizListFragment.STRING_POOL;
 
 public class EditMapsActivityTest {
     @Rule
