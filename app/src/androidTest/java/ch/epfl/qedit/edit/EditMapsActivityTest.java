88d037437178928b5bfe3932dfb5b790a9f6b6a0
cjordan7 <cosmehbvjordan@gmail.com>
2020-05-14 23:52:14 +0200

    More tests

--- app/src/androidTest/java/ch/epfl/qedit/edit/EditMapsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/qedit/edit/EditMapsActivityTest.java
@@ -3,11 +3,15 @@ package ch.epfl.qedit.edit;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.Intent;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+
+import ch.epfl.qedit.R;
 import ch.epfl.qedit.view.edit.EditMapsActivity;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,7 +28,13 @@ public class EditMapsActivityTest {
     }
 
     @Test
-    public void testThatFragmentsAreDisplayed() {
+    public void testThatFActivityIsDisplayed() {
         onView(withText("Map")).check(matches(isDisplayed()));
+        onView(withId(R.id.map)).check(matches(isDisplayed()));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testErrorOnMapNull() {
+        testRule.getActivity().onMapReady(null);
     }
 }
