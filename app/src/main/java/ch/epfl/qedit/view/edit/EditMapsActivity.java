ae50bdf78af1d6853bd2bb0b9c4a16de167873a4
cjordan7 <cosmehbvjordan@gmail.com>
2020-05-14 16:51:43 +0200

    Changes

--- app/src/main/java/ch/epfl/qedit/view/edit/EditMapsActivity.java
+++ app/src/main/java/ch/epfl/qedit/view/edit/EditMapsActivity.java
@@ -1,11 +1,11 @@
 package ch.epfl.qedit.view.edit;
 
-import androidx.fragment.app.FragmentActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import ch.epfl.qedit.R;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,17 +14,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Objects;
-
-import ch.epfl.qedit.R;
-import ch.epfl.qedit.model.User;
-
-import static ch.epfl.qedit.view.LoginActivity.USER;
-
-public class EditMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class EditMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     public static final String LATITUDE = "ch.epfl.qedit.view.edit.LATITUDE";
     public static final String LONGITUDE = "ch.epfl.qedit.view.edit.LONGITUDE";
-    private final String yourLocation = getString(R.string.your_location);
 
     private GoogleMap mMap;
     private LatLng latLng;
@@ -33,47 +25,66 @@ public class EditMapsActivity extends FragmentActivity implements OnMapReadyCall
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_maps);
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        Intent intent = getIntent();
-        double latitude = Objects.requireNonNull(intent.getExtras()).getDouble(LATITUDE);
-        double longitude = Objects.requireNonNull(intent.getExtras()).getDouble(LONGITUDE);
+        // This below will be the default location if user refuse location permission
+        // TODO in future PR: get current location
+        double latitude = 46.518442;
+        double longitude = 6.561983;
         latLng = new LatLng(latitude, longitude);
     }
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Manipulates the map once available. This callback is triggered when the map is ready to be
+     * used. This is where we can add markers or lines, add listeners or move the camera. In this
+     * case, we just add a marker near Sydney, Australia. If Google Play services is not installed
+     * on the device, the user will be prompted to install it inside the SupportMapFragment. This
+     * method will only be triggered once the user has installed Google Play services and returned
+     * to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        mMap.addMarker(new MarkerOptions().position(latLng).title(yourLocation));
+        // Add a marker on your current position and move the camera
+        mMap.addMarker(
+                new MarkerOptions().position(latLng).title(getString(R.string.your_location)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         CameraUpdate latLngLocation = CameraUpdateFactory.newLatLngZoom(latLng, 12);
         mMap.animateCamera(latLngLocation);
 
-        mMap.setOnMapClickListener(point -> {
-            mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(point));
-        });
+        mMap.setOnMapClickListener(
+                point -> {
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(point));
+                });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.map_menu, menu);
+        // TODO in future PR: Add a new item to go back to current location
+        getMenuInflater().inflate(R.menu.edit_menu, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        // TODO in future PR: implement menu options
+        switch (id) {
+            case R.id.next:
+            case R.id.previous:
+                break;
+            case R.id.overview:
+                break;
+            case R.id.done:
+                break;
+        }
+
+        return true;
+    }
 }
