5d98efcfb92c682b3f8e91971d9c99c69643a4a5
Alexis <alexis.p.cohen@gmail.com>
2020-05-21 15:21:38 +0200

    removed empty test class

--- app/src/androidTest/java/ch/epfl/qedit/QR/MockScannerTest.java
+++
@@ -1,7 +0,0 @@
-package ch.epfl.qedit.QR;
-
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import org.junit.runner.RunWith;
-
-@RunWith(AndroidJUnit4ClassRunner.class)
-public class MockScannerTest {}
