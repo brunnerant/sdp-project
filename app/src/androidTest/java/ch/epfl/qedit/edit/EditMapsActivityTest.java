805270149fc06786f301d5e10bd2c87030700a5a
Nicolas Bähler <nicolas.bahler@epfl.ch>
2020-05-21 22:12:46 +0200

    Remove label test for map activity

--- app/src/androidTest/java/ch/epfl/qedit/edit/EditMapsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/qedit/edit/EditMapsActivityTest.java
@@ -4,7 +4,6 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.Intent;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
