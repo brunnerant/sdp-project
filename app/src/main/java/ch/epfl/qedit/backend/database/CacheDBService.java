63215145bf4ba257d9c36b00afa51f0f25d023b6
Antoine Brunner <antoine.brunner@epfl.ch>
2020-06-02 13:40:58 +0200

    Fix issue with List not being serializable

--- app/src/main/java/ch/epfl/qedit/backend/database/CacheDBService.java
+++ app/src/main/java/ch/epfl/qedit/backend/database/CacheDBService.java
@@ -10,9 +10,11 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -27,13 +29,19 @@ public class CacheDBService implements DatabaseService {
     // This is the underlying database service that is being decorated
     private final DatabaseService dbService;
 
-    // This it the cache directory
+    // These are the directories in which we cache the data
     private final File userCacheDir;
+    private final File quizCacheDir;
+    private final File poolCacheDir;
+    private final File langCacheDir;
 
     /** Creates a cache that decorates the given database service */
     public CacheDBService(DatabaseService dbService, Context context) {
         this.dbService = dbService;
         this.userCacheDir = new File(context.getCacheDir(), "users");
+        this.quizCacheDir = new File(context.getCacheDir(), "quizzes");
+        this.poolCacheDir = new File(context.getCacheDir(), "pools");
+        this.langCacheDir = new File(context.getCacheDir(), "languages");
 
         // Create an empty directory if it does not already exist
         if (!userCacheDir.exists()) userCacheDir.mkdirs();
@@ -41,17 +49,27 @@ public class CacheDBService implements DatabaseService {
 
     @Override
     public CompletableFuture<List<String>> getQuizLanguages(String quizId) {
-        return dbService.getQuizLanguages(quizId);
+        // Lists are not serializable, but array lists are, so we just need to wrap the lists
+        // into array lists before storing them in the cache. Since array lists are lists,
+        // nothing needs to be done to deserialize.
+        return retrieve(
+                new File(langCacheDir, quizId),
+                () -> dbService.getQuizLanguages(quizId),
+                list -> new ArrayList<>(list),
+                list -> list // cannot use Function.identity() because of the type system
+                );
     }
 
     @Override
     public CompletableFuture<Quiz> getQuizStructure(String quizId) {
-        return dbService.getQuizStructure(quizId);
+        return retrieve(new File(quizCacheDir, quizId), () -> dbService.getQuizStructure(quizId));
     }
 
     @Override
     public CompletableFuture<StringPool> getQuizStringPool(String quizId, String language) {
-        return dbService.getQuizStringPool(quizId, language);
+        return retrieve(
+                new File(quizCacheDir, quizId + "-" + language),
+                () -> dbService.getQuizStringPool(quizId, language));
     }
 
     @Override
@@ -127,30 +145,48 @@ public class CacheDBService implements DatabaseService {
     }
 
     /**
-     * Performs a retrieval from the cache or the database, depending on their availability. The
+     * * Performs a retrieval from the cache or the database, depending on their availability. The
      * data is first searched in the cache. If it is not present there, it is fetched from the
      * database and stored in the cache for the next retrievals.
+     *
+     * @param cache the file in which the data should be stored
+     * @param database a function that requests the data from the database, if needed
+     * @param serialize a function that serializes the data before file storage
+     * @param deserialize a function that deserializes the data when being loaded from the cache
+     * @param <S> the type of the data
+     * @param <T> the type of the data, once serialized to the cache
+     * @return a future that complete once the full retrieval was performed
      */
-    private <T extends Serializable> CompletableFuture<T> retrieve(
-            File cache, Supplier<CompletableFuture<T>> database) {
+    private <S, T extends Serializable> CompletableFuture<S> retrieve(
+            File cache,
+            Supplier<CompletableFuture<S>> database,
+            Function<S, T> serialize,
+            Function<T, S> deserialize) {
         return lookup(cache)
                 .thenCompose(
                         fromCache -> {
                             if (fromCache != null) {
                                 // If the data is already available, we can return it immediately
-                                return CompletableFuture.<T>completedFuture(fromCache);
+                                return CompletableFuture.<S>completedFuture(
+                                        deserialize.apply((T) fromCache));
                             } else {
                                 // Otherwise, we fetch it from the real database
-                                CompletableFuture<T> f = database.get();
+                                CompletableFuture<S> f = database.get();
 
                                 // Once it arrives, we cache it locally
                                 f.thenAccept(
                                         fromDB -> {
-                                            storeInCache(cache, fromDB);
+                                            storeInCache(cache, serialize.apply(fromDB));
                                         });
 
                                 return f;
                             }
                         });
+    }
+
+    /** Same version as above, when the data type is already serializable */
+    private <T extends Serializable> CompletableFuture<T> retrieve(
+            File cache, Supplier<CompletableFuture<T>> database) {
+        return retrieve(cache, database, Function.identity(), Function.identity());
     }
 }
